1be0b06fa4055d62fc4e423043f1cb051b328704
ghostb0x <ghostb0x.github@gmail.com>
2023-05-23 18:13:09 +0200

    steps 3 and 4 - updated scroll bars on menus and added background color bar

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,7 +30,9 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.backgroundColorBar}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
